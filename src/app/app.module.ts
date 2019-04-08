fc4e2698799f752a790d9f101f8b3b0966775d99
Shafaiatul Islam <sislam@horizonmedia.com>
2019-04-08 14:43:32 -0400

    Initial commit and intigrate Angular Material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,19 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+
+
+// 3rd party module
+import { AngularMaterialModule } from './angular-material.module';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AngularMaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
